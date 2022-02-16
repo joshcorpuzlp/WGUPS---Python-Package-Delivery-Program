6744bb6918762db27f58c138bd143b5acd630602
jshcrpz1001 <95312016+jshcrpz1001@users.noreply.github.com>
2022-02-16 10:03:09 -0600

    Graph Class and Loaded distance data

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from hashMap import HashMap
+from turtle import distance
+from HashMap import HashMap
+from Graph import Graph
 from package import Package
 import csv
 
@@ -17,7 +19,7 @@ def loadPackageData(fileName):
         package_data = list(reader)
 
         # create a package object using each row of from package_list
-        for i in range(1, len(package_data)):
+        for i in range(1, len(package_data)):  # skipped the first row of headers
             current_package = Package()
             current_package.id = package_data[i][0]
             current_package.address = package_data[i][1]
@@ -30,26 +32,61 @@ def loadPackageData(fileName):
             current_package.status = ""
 
             # add the package object in the list
-            package_list.insert(
+            package_list.insertById(
                 current_package.id, current_package)
 
 
 # call the loadPackageData to store the csv data into the declared hashmap
 loadPackageData("wgups_package_file.csv")
 
-# test to see if hashmap works
-package_1 = package_list.search("10")
-package_2 = package_list.search("11")
-package_3 = package_list.search("20")
+# tests to see if hashmap works
+# package_1 = package_list.searchById("10")
+# package_2 = package_list.searchById("11")
+# package_3 = package_list.searchById("20")
+# print(package_1)
+# print(package_2)
+# print(package_3)
+# print(package_1.address)
+# print(package_3.address)
+# print(package_list.hash_table)
+# package_list.printAllItems()
+# for i in range(package_list.size):
+#     print(package_list.hash_table[i])
 
-print(package_1)
-print(package_2)
-print(package_3)
 
-print(package_1.address)
-print(package_3.address)
+# TODO: Using the Graph class:
+# 1. Load the addresses in Column A in the Graph's vertex list
+# 2. Load the address_2 it's corresponding distance to address_1 as a 2-item list appended to a list of
 
-# for i in range(len(package_list.hash_table)):
-#     print(package_list.searchByIndex(i))
+distanceGraph = Graph()
 
-print(package_list.hash_table)
+
+def loadDistanceData(fileName):
+    rows = []
+    with open(fileName) as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            rows.append(row)
+
+    # for row in rows:
+    #     print(row, "\n")
+
+    # print(rows)
+
+    # load the addresses to the vertex list
+    for row in rows:
+        distanceGraph.addVertex(row[0])
+
+    # # load contents for adjacency list
+    for i in range(len(distanceGraph.vertex_list_a)):
+        for j in range(len(distanceGraph.vertex_list_a)):
+            distanceGraph.addEdgeWeights(
+                distanceGraph.vertex_list_a[i], distanceGraph.vertex_list_a[j], rows[i][j+1])
+
+
+fileName = "wgups_distance_table.csv"
+loadDistanceData(fileName)
+# distanceGraph.getGraph()
+#distanceGraph.getEdgeWeights('1330 2100 S\n(84106)')
+distanceGraph.printEdgeWeights()
