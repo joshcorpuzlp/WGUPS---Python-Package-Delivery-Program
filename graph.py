8f116c21ebfb06881b004495a68541c15d19728d
jshcrpz1001 <95312016+jshcrpz1001@users.noreply.github.com>
2022-02-16 10:02:42 -0600

    finished Graph class and loaded data to graph

--- graph.py
+++ graph.py
@@ -1,15 +1,51 @@
 # Graph class will represent the matrix table of distance tables
-# it needs to have a list of vertices: Address (Adjacency list)
-# a 2d array of edge_list
+# it needs to have a list of vertices: Address
+# it will have a dictionary of {address_a: [address_b, weight], [address_b, weight], .... }
+# key = vertex_a; values = list[[vertex_b, distance]]
 
 class Graph():
     def __init__(self):
-        # initialize an empty dictionary that will hold vertices. In this case it would be the locations
-        self.vertex_list = {}
-        # initialize an empty list that will hold a key value pair key: (location_a, location b) value: weights (distance)
+        # initialize an empty list that will hold the vertices. In this case it would be the locations
+        self.vertex_list_a = []
+        self.vertex_list_b = self.vertex_list_a
+        # initialize an empty list that will hold a key value pair key = (location_a) and value = [location_b, weight]
         self.edge_weights = {}
 
-    # To do: create a method that will load the a vertex_list each with its own
+    # Adds a node in the vertex list
     def addVertex(self, vertex):
-        # each vertex should have a list of distances
-        self.vertex_list[vertex] = []
+        # check if the vertex is not in the vertex_list already
+        if vertex not in self.vertex_list_a:
+            self.vertex_list_a.append(vertex)
+        else:
+            print("Vertex: ", vertex, " is in the vertex list already")
+
+    def addEdgeWeights(self, vertex_a, vertex_b, weight):
+        temp = []
+        if vertex_a in self.vertex_list_a and vertex_b in self.vertex_list_a:
+
+            if vertex_a not in self.edge_weights:
+                temp.append([vertex_b, weight])
+                self.edge_weights[vertex_a] = temp
+
+            elif vertex_a in self.edge_weights:
+                temp.extend(self.edge_weights[vertex_a])
+                temp.append([vertex_b, weight])
+                self.edge_weights[vertex_a] = temp
+
+        else:
+            print("Nodes are not part of the vertex list")
+
+    def printVertexList(self):
+        for i in range(len(self.vertex_list_a)):
+            print(self.vertex_list_a[i])
+
+    def printEdgeWeights(self):
+        for vertex in self.vertex_list_a:
+            print("key: ", vertex, "values: ", self.edge_weights[vertex], "\n")
+
+    def getEdgeWeights(self, vertex_a):
+        print(self.edge_weights[vertex_a])
+
+    def getGraph(self):
+        for node in (self.edge_weights):
+            print(node, "--->", [i for i in self.edge_weights[node]], "\n")
--- hashMap.py
+++ hashMap.py
@@ -1,11 +1,15 @@
+# might be a good idea to try and use a different hash_map
+# Hash_map will be used for storing all the package objects
+
 class HashMap():
     def __init__(self, size):
         self.size = size
         self.hash_table = self.create_buckets()
 
     def create_buckets(self):
-        return [[] for i in range(self.size)]
+        return [[] for _ in range(self.size)]
 
+    # The hash key is calculated by the sum of the char values of the key % the size of the hashmap
     def __getHashKey(self, key):
         hash_key = 0
         for char in key:
@@ -13,6 +17,7 @@ class HashMap():
 
         return hash_key % self.size
 
+    # method inserts a key value pair to the hashmap
     def insert(self, key, value):
 
         # Create the hash_key from from passed key
@@ -35,7 +40,35 @@ class HashMap():
         else:
             self.hash_table[hash_key].append([key, value])
 
-    # does not work
+    # creates a hash key by converting the id (string) to an integer
+    def __convertIdToHashKey(self, key):
+        return int(key)
+
+    # same logic as insert but instead of creating the hash_key function, it just uses the package id as the hash_function
+    def insertById(self, key, value):
+        hash_key = self.__convertIdToHashKey(key)
+
+        if (self.hash_table[hash_key]):
+            for pair in self.hash_table[hash_key]:
+                if pair[0] == hash_key:
+                    pair[1] = value
+                    return True
+                else:
+                    self.hash_table[hash_key].append([key, value])
+        else:
+            self.hash_table[hash_key].append([key, value])
+
+    # Method removes an item from th hashmap
+
+    def remove(self, key):
+        hash_key = self.__getHashKey(key)
+        if not self.hash_table[hash_key]:
+            return False
+        else:
+            for i in range(len(self.hash_table[hash_key])):
+                if self.hash_table[hash_key][i][0] == key:
+                    self.hash_table[hash_key].pop(i)
+                    return True
 
     def search(self, key):
         hash_key = self.__getHashKey(key)
@@ -48,31 +81,27 @@ class HashMap():
 
             return None
 
-    def searchByIndex(self, index):
-        if self.hash_table[index]:
-            for pair in self.hash_table[index]:
-                return pair[1]
-        else:
-            pass
-
-    def getAllItems(self):
-        for kvp in self.hash_table:
-            return kvp[1]
-
-    def remove(self, key):
-        hash_key = self.__getHashKey(key)
+    def searchById(self, key):
+        hash_key = self.__convertIdToHashKey(key)
         if not self.hash_table[hash_key]:
-            return False
+            return None
         else:
-            for i in range(len(self.hash_table[hash_key])):
-                if self.hash_table[hash_key][i][0] == key:
-                    self.hash_table[hash_key].pop(i)
-                    return True
+            for pair in self.hash_table[hash_key]:
+                if pair[0] == key:
+                    return pair[1]
 
-    # checkers
-    def printAll(self):
-        for pair in self.hash_table:
-            print(pair)
+            return None
 
-    def printList(self):
-        print(self.hash_table)
+    # # does not work
+    # def searchByIndex(self, index):
+    #     if self.hash_table[index]:
+    #         for pair in self.hash_table[index]:
+    #             return pair[1]
+    #     else:
+    #         pass
+
+    # prints all items within the hashtable
+    def printAllItems(self):
+        for bucket in self.hash_table:
+            for kvp in bucket:
+                print(kvp[1])
